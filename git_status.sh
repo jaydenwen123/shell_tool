c222be182614566feaaba9d1ee6b62d3aaacf74a
jaydenwen123 <2282186474@qq.com>
2020-07-07 22:45:22 +0800

    feat:add gs show the git url and copyed into pasteboard

--- git_status.sh
+++ git_status.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-git status
+if [ $# -eq 0 ];then
+	git status
+	exit 0
+fi
+
+projectUrl=`git remote -v |awk '{print $2}'|head -1`
+echo  -e "[INFO] the project url:\n"$projectUrl
+echo $projectUrl |pbcopy
+echo "[INFO] the project url is copyed in your pasteboard"
