cb935bb129c2bc226415a3db2a62c95b40c2b307
jaydenwen123 <2282186474@qq.com>
2020-07-07 21:43:54 +0800

    feat:update git_push.sh

--- git_push.sh
+++ git_push.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# current branch
+branch=`git branch |grep "*"|awk '{print $2}'`
+
+if [  $# -eq 0   ];then
+	#read -p "please input the pushed branch,the default branch is master:" -t 30 branch
+	#if [  ${#branch} -eq 0  ];then
+	#	branch="master"
+	#fi
+	echo "[INFO] execute action:<git push origin ${branch}>"
+	git push origin $branch
+	exit 0
+fi
+
 echo "[INFO] git status"
 git status
 echo "[INFO] exec git add ."
@@ -7,6 +20,6 @@ git status
 echo "[INFO] exec git commit -m $1"
 git commit -m "$1"
 if [ $# -lt 2 ];then
-	echo "[INFO] git push origin master"
-	git push origin master
+	echo "[INFO] git push origin ${branch}"
+	git push origin ${branch}
 fi
